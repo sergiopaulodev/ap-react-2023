c1b26f2f677a900c7ec9258a13689d1936ce6d8f
Sergio Torres Fernández <spaulomusic@gmail.com>
2023-11-27 09:48:17 -0300

    init-project

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,8 @@
+
+export default function TaskItem() {
+  return (
+    <div>
+      <p>tarea1</p>
+    </div>
+  );
+}
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+import TaskItem from './TaskItem'
+
+export default function TaskList() {
+  return (
+    <>
+        <ul>
+            <li><TaskItem/></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </>
+  );
+}
